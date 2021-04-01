17ec48907ef4ae929102329098c0a0307f879450
MalteHolmer-BD <malte.holmer@beiersdorf.com>
2021-04-01 22:24:32 +0200

    Now the Segelbar has a lot of comments while drinking!

--- nebelhorn/bot.py
+++ nebelhorn/bot.py
@@ -5,11 +5,12 @@ from discord.ext import commands
 from dotenv import load_dotenv
 import asyncio
 import random
+from comments import comments
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
-bot = commands.Bot(command_prefix="!")
+bot = commands.Bot(command_prefix="#")
 
 class Nebelhorn(commands.Cog):
 
@@ -18,6 +19,7 @@ class Nebelhorn(commands.Cog):
         self.honk_loop_task = None
         self.min_minutes = 5
         self.max_minutes = 25
+        self.last_channel = None
 
     async def honk_at_random_loop(self):
         min_seconds = self.min_minutes*60
@@ -28,6 +30,7 @@ class Nebelhorn(commands.Cog):
             await self.play_honk()
 
     async def play_honk(self):
+        await self.honk_comment()
         audio = discord.FFmpegPCMAudio("./Nebelhorn.mp3")
         if bot.voice_clients:
             bot.voice_clients[0].play(audio)
@@ -41,15 +44,15 @@ class Nebelhorn(commands.Cog):
         if self.honk_loop_task and not self.honk_loop_task.cancelled():
             self.honk_loop_task.cancel()
 
-    @commands.command()
-    async def set_honk_timing(self, ctx: commands.context.Context, min_minutes, max_minutes):
+    @commands.command(brief="Setzt das Intervall in dem wir wohl trinken müssen.")
+    async def set_timer(self, ctx: commands.context.Context, min_minutes, max_minutes):
         try:
             min_minutes = float(min_minutes)
             max_minutes = float(max_minutes)
         except ValueError:
             await ctx.channel.send("Intervall ergibt keinen Sinn, willst du gar nicht trinken?")
             return
-        if min_minutes >= max_minutes:
+        if min_minutes >= max_minutes or min_minutes < 0 or max_minutes < 0:
             await ctx.channel.send("Intervall ergibt keinen Sinn, willst du gar nicht trinken?")
             return
         self.min_minutes = min_minutes
@@ -59,43 +62,49 @@ class Nebelhorn(commands.Cog):
         if self.honk_loop_task and not self.honk_loop_task.cancelled():
             await self.start_honk()
 
-    @commands.command()
-    async def show_honk_status(self, ctx):
+    @commands.command(brief="Guck woran du mit der Segelbar heut bist.")
+    async def whatsup(self, ctx):
         await ctx.channel.send("Intervall ist momentan: {}-{} Minuten.".format(self.min_minutes, self.max_minutes))
         await ctx.channel.send("Der Honker ist momentan {}."
                                .format("aktiviert" if (self.honk_loop_task and not self.honk_loop_task.cancelled())
                                        else "aus"))
 
-    @commands.command()
+    @commands.command(brief="Lässt den Spaß beginnen ;)")
     async def start_honker(self, ctx: commands.context.Context):
+        self.last_channel = ctx.channel
         if not bot.voice_clients:
             await ctx.channel.send("Die Segelbar kann nur honken, wenn sie in einem Voice-Channel ist.")
             return
         await ctx.channel.send("Honk-Timer wurde gestartet. Bald müssen wir trinken ;)")
         await self.start_honk()
 
-    @commands.command()
+    @commands.command(brief="Hält die Segelbar davon ab uns noch mehr trinken zu lassen.")
     async def stop_honker(self, ctx: commands.context.Context):
         await self.stop_honk()
         await ctx.channel.send("Honk-Timer gestoppt, aber wieso?????")
 
-    @commands.command()
+    async def honk_comment(self):
+        await self.last_channel.send(random.choice(comments))
+
+    @commands.command(brief="Lässt die Segelbar ihr Nebelhorn spielen.")
     async def honk(self, ctx: commands.context.Context):
         if not bot.voice_clients:
             await ctx.channel.send("Die Segelbar kann nur honken, wenn sie in einem Voice-Channel ist.")
             return
 
+        self.last_channel = ctx.channel
         await self.play_honk()
-        await ctx.channel.send("Honk-Timer wurde zurückgesetzt.")
+
+        #await ctx.channel.send("Honk-Timer wurde zurückgesetzt.")
         if self.honk_loop_task and not self.honk_loop_task.cancelled():
             await self.start_honk(ctx)
 
-    @commands.command()
+    @commands.command(brief="Lässt die Segelbar zu uns kommen :)")
     async def join(self, ctx: commands.context.Context):
         if ctx.author.voice:
             await ctx.author.voice.channel.connect()
 
-    @commands.command()
+    @commands.command(brief="Weg mit dir Segelbar :O")
     async def leave(self, ctx: commands.context.Context):
         await self.stop_honker(ctx)
         if bot.voice_clients:
---
+++ nebelhorn/comments.py
@@ -0,0 +1,41 @@
+comments = ["SAUFEN!!!",
+            "Von der Mitte zur Titte, zum Sack - ZACK ZACK"
+            "Bist du nachher trotzdem blind, war zu stark der Gegenwind."
+            "Chemisch gesehen ist Alkohol eine Lösung.",
+            "Zwischen Leber und Milz passt immer noch ein Pils.",
+            "Bier ist lecker. Bier ist toll. Am liebsten bin ich voll.",
+            "Unser letzter Wille, immer mehr Promille.",
+            "Lass dich nicht Lumpen, HOCH DEN HUMPEN!",
+            "Das Leben ist an manchen Tagen halt nur im Vollrausch zu ertragen...",
+            "Wer Hopfen säht, wird Bier ernten.",
+            "Säufste, Strirbste, Säufste nicht, Stirbste auch ... Also Säufste!",
+            "Bier unser, das du bist im Glase, gesegnet werde dein Erfinder, mein"
+            " Rausch komme, dein Wille geschehe, wie im Himmel - so auch in der Kneipe."
+            " Unser Durst still uns heute, und vergib uns unsere Schulden, wie auch wir"
+            " vergeben unseren Gläubigern. Und führe uns nicht in die Milchbar, sondern"
+            " gib uns die Kraft weiter zu trinken, denn dein ist der Durst, der Rausch"
+            " und die Seligkeit in Ewigkeit. Prost!",
+            "Der GLOMPF ist mein Drink, mir wird nichts mangeln.\nEr weidet mich auf einer grünen Aue\n"
+            "Und führet mich zu frischem Gin.\nEr erquicket meine Seele.\n"
+            "Er führet mich auf rechter Straße, um des Limettensaftes willen.\n"
+            "Und ob ich schon wanderte im finsteren Kirchental,\n"
+            "fürchte ich kein Unglück, denn du bist bei mir.Dein Orangen - und Maracujasaft trösten mich.\n"
+            "Du bereitest vor mir einen Tisch im Angesicht eines Torbens.\n"
+            "Du salbest sein Haupt mit GLOMPF und schenkest ihm immer voll ein.\n"
+            "Pfirsichlikör und Ficken werden mir folgen mein Leben lang,\n"
+            "und ich werde bleiben im Hause des Torbens immerdar.",
+            "Prost ihr Säcke!",
+            "Fängst du Mittags an zu saufen, kannst du abends nicht mehr laufen.",
+            "Nicht lang schacken, Kopf in Nacken.",
+            "Hau weg die Scheiße.",
+            "Sport ist mord, nur Sprit hält fit.",
+            "Das Flüssige muss ins Durstige.",
+            "Niemals vergessen, sechs Bier sind auch ein Essen!",
+            "Morgens ein Bier und der Tag gehört dir.",
+            "Alkohol löst keine Probleme... aber das tut Wasser auch nicht ;)",
+            "Eine Kiste Bier hat 24 Flaschen, ein Tag hat 24 Stunden, Zufall?!?",
+            "Auf Ex!!!",
+            "Wein auf Bier, das rat ich dir. Bier auf Wein, das lass sein.",
+            "Das erste Bier, das löscht den Durst. Ein zweites stimmt mich heiter. Nach dreien ist mir alles Wurst, drum sauf ich einfach weiter.",
+            "Melkt der Bauer seinen Stier, trank der Trottel zu viel Bier.",
+            "Jeder muss an etwas glauben. Ich glaub, ich trink noch Einen!"]
